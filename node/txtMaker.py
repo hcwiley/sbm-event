20ccf910b660e3424d277180d3726511903bf9d9
H. Cole Wiley <hcwiley@gmail.com>
2013-01-25 00:14:29 -0600

    fixed the lowerCase and jpeg issues

--- node/txtMaker.py
+++ node/txtMaker.py
@@ -1,9 +1,16 @@
 import os, sys
 from glob import glob
 os.chdir(sys.argv[1])
-for f in glob("*.*"):
-  file = open(f.lower().replace(".jpg",".txt"), "w+")
-  file.write("hello world. <strong>i'm bold text</strong>. <italic>i'm italics text</italic>")
-  file.flush()
-  file.close()
-  os.rename(f, "%s" % f.lower())
+imageTypes = ("*.png", "*.PNG", "*.jpg",
+        "*.JPG", "*.jpeg", "*.JPEG")
+files_grabbed = []
+for files in imageTypes:
+    files_grabbed.extend(glob(files))
+for f in files_grabbed:
+  fType = f.split(".")
+  fType = fType[len(fType) - 1]
+  file = open(f.replace(fType, "txt"), "w+")
+  #file.write("hello world. <strong>i'm bold text</strong>. <italic>i'm italics text</italic>")
+  #file.flush()
+  #file.close()
+  os.rename(f, "%s" )#% f.lower())
